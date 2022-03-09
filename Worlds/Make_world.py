a129206dbbd51d40d722cbeddaad0e687dc87139
Shoham <sohambensimhon634@gmail.com>
2022-03-09 18:36:40 +0200

    world making algorithm

---
+++ Worlds/Make_world.py
@@ -0,0 +1,20 @@
+import random
+
+
+def make_world(width, height, world_name):
+    f = open(world_name + ".txt", 'w')
+    for i in range(height):
+        for j in range(width):
+            if j == width - 1:
+                f.write("G")
+            else:
+                if random.randint(1, 10) >= 5:
+                    f.write("G ")
+                else:
+                    f.write("G2 ")
+        if i != height - 1:
+            f.write("\n")
+    f.close()
+
+
+make_world(250, 250, "World1")
