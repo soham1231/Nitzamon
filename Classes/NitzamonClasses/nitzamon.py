109145af98a1e4c8d9387292ce2adf72eaf0bd19
GiladCohe <giladco31@gmail.com>
2022-03-09 19:23:21 +0200

    Nitzamon classes im gay

---
+++ Classes/NitzamonClasses/nitzamon.py
@@ -0,0 +1,12 @@
+
+
+class Nitzamon:
+
+    def __init__(self, element, dmg, hp, spd, sprite, list_of_moves, name):
+        self.name = name
+        self.element = element
+        self.dmg = dmg
+        self.hp = hp
+        self.spd = spd
+        self.sprite = sprite
+        self.list_of_moves = list_of_moves
