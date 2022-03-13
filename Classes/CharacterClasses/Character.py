f83be7abd4d23e4d9d3484d3561da99c9162e06f
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 21:20:04 +0200

    Can walk under leaves

--- Classes/CharacterClasses/Character.py
+++ Classes/CharacterClasses/Character.py
@@ -10,5 +10,13 @@ class Character:
         self.world = world
 
     def draw(self, camera_pos):
-        WIN.blit(self.sprite, (self.pos[0] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
-                               self.pos[1] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
+
+        if self.world[self.pos[1]][self.pos[0]] == "l":
+            # WIN.blit(self.sprite, (self.pos[1] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
+            #                        self.pos[0] * Constants.SCALE - (camera_pos[1] * Constants.SCALE))) # Should use that if we make leaves transparent
+            tile = Constants.TILES[self.world[self.pos[0]][self.pos[1]]]
+            WIN.blit(tile, (self.pos[1] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
+                            self.pos[0] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
+        else:
+            WIN.blit(self.sprite, (self.pos[0] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
+                                   self.pos[1] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
