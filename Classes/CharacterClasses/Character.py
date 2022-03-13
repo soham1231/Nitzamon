30e10c89c4b579386e68cf6db398a2bb80d70c37
Shoham <sohambensimhon634@gmail.com>
2022-03-13 22:57:48 +0200

    Leaves are now transparent when you walk under them

--- Classes/CharacterClasses/Character.py
+++ Classes/CharacterClasses/Character.py
@@ -11,12 +11,22 @@ class Character:
 
     def draw(self, camera_pos):
 
+        tile = Constants.TILES["l"]
         if self.world[self.pos[1]][self.pos[0]] == "l":
-            # WIN.blit(self.sprite, (self.pos[1] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
-            #                        self.pos[0] * Constants.SCALE - (camera_pos[1] * Constants.SCALE))) # Should use that if we make leaves transparent
-            tile = Constants.TILES[self.world[self.pos[0]][self.pos[1]]]
-            WIN.blit(tile, (self.pos[1] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
-                            self.pos[0] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
+
+            # Grass underneath the leaves
+            WIN.blit(Constants.TILES["G"], (self.pos[0] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
+                                            self.pos[1] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
+
+            # Player
+            WIN.blit(self.sprite, (self.pos[0] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
+                                   self.pos[1] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
+            # Changing leaf transparency and drawing it
+            tile.set_alpha(200)
+            WIN.blit(tile, (self.pos[0] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
+                            self.pos[1] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
         else:
+            # Return the leaf to normal transparency
+            tile.set_alpha(255)
             WIN.blit(self.sprite, (self.pos[0] * Constants.SCALE - (camera_pos[0] * Constants.SCALE),
                                    self.pos[1] * Constants.SCALE - (camera_pos[1] * Constants.SCALE)))
