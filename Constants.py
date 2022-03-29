31da522df3c84f61d32a42be0a748f85d0cd4bc0
BuildTools <unconfigured@null.spigotmc.org>
2022-03-29 18:46:23 +0300

    someone changed the fullscreen??

--- Constants.py
+++ Constants.py
@@ -1,16 +1,8 @@
 import pygame
-import os
 from math import ceil
 
-# Pygame screen variables
-os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
 pygame.init()
-
-# now let's see how big our screen is
-info = pygame.display.Info()
-
-# and create a borderless window that's as big as the entire screen
-WIN = pygame.display.set_mode((info.current_w, info.current_h), pygame.NOFRAME)
+WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 # WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 X, Y = WIN.get_size()
