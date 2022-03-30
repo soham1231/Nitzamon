a0870e6fc20b4dfa83e2de3a5348e446a2a4df30
Shoham <sohambensimhon634@gmail.com>
2022-03-30 19:33:12 +0300

    fixed bug

--- Main.py
+++ Main.py
@@ -66,8 +66,8 @@ def draw_npcs(npc_list, camera_pos):  # NUMBER OF N'S ON THE MAP MUST BE EQUAL T
 
 
 def random_move():
-    element = random.choice([Constants.FIRE, Constants.WATER, Constants.EARTH])
-    dmg = random.randint(1, 10)
+    element = random.choice(["fire", "water", "earth"])
+    dmg = random.choice([1, 2, 3, 4])
     name = random.choice(["hug", "kiss", "pet", "touch"])
     return Move.Move(element, dmg, name)
 
@@ -107,7 +107,6 @@ def main():
         nitzamon_list.append(random_nitzamon())
     for i in range(3):
         equipped.append(random_nitzamon())
-    equipped[0].spd = 10000
     player = Player.Player("Shoham", Constants.PLAYER_IMAGE, [1, 1], equipped, nitzamon_list, 0, world)
     nitzamon_pressed = None
     player.camera()
@@ -131,7 +130,7 @@ def main():
                 if fight_menu.in_fight:
                     fight_menu.run(pygame.mouse.get_pos())
 
-                    if fight_menu.topRight_rect.collidepoint(pygame.mouse.get_pos()) and not fight_menu.attacking and fight_menu.playerTurn:
+                    if fight_menu.topRight_rect.collidepoint(pygame.mouse.get_pos()) and not fight_menu.attacking:
                         fight_menu.changing_nitzamons = True
 
                     if fight_menu.changing_nitzamons:
@@ -139,12 +138,10 @@ def main():
                         if replacing is not None:
                             fight_menu.change_nitzamons(replacing)
                             fight_menu.changing_nitzamons = False
-
-                    if fight_menu.topLeft_rect.collidepoint(pygame.mouse.get_pos()) and fight_menu.playerTurn:
-                        fight_menu.attacking = True
-
                     if fight_menu.attacking and fight_menu.playerTurn:
                         fight_menu.attack(pygame.mouse.get_pos())
+                    if fight_menu.topLeft_rect.collidepoint(pygame.mouse.get_pos()):
+                        fight_menu.attacking = True
 
                 if Inventory.is_open and not (Inventory.info_open or Inventory.equip_open):
                     nitzamon_pressed = Inventory.check_collision(pygame.mouse.get_pos(), player.nitzamon_bag)
